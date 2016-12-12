0dfd0f6de7aae7890b4b08679fdc74bef34575a2
Chris Richards <chris.richards@yellowfeather.co.uk>
2016-12-12 15:48:33 +0000

    Adds DbfTableTests

---
+++ test/DbfReader.Tests/DbfTableTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace DbfReader.Tests
+{
+    [Collection("dbase_30")]
+    public class DbfTableTests
+    {
+        [Fact]
+        public void Should_dispose_binary_reader()
+        {
+            const string fixturePath = "./test/fixtures/dbase_30.dbf";
+
+            var dbfTable = new DbfTable(fixturePath);
+
+            dbfTable.BinaryReader.ShouldNotBeNull();
+
+            dbfTable.Dispose();
+
+            dbfTable.BinaryReader.ShouldBeNull();
+        }
+
+        [Fact]
+        public void Should_dispose_memo()
+        {
+            const string fixturePath = "./test/fixtures/dbase_30.dbf";
+
+            var dbfTable = new DbfTable(fixturePath);
+
+            dbfTable.Memo.ShouldNotBeNull();
+
+            dbfTable.Dispose();
+
+            dbfTable.Memo.ShouldBeNull();
+        }
+    }
+}
