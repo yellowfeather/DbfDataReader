b918078d09826836b0bda2838af3302a6d9b1a36
Chris Richards <chris.richards@yellowfeather.co.uk>
2021-07-06 11:20:13 +0100

    Adds MemoryDiagnoser to benchmarks

--- test/DbfDataReader.Benchmarks/Program.cs
+++ test/DbfDataReader.Benchmarks/Program.cs
@@ -1,11 +1,16 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
 
 namespace DbfDataReader.Benchmarks
 {
     public class Program
     {
-        public static void Main(string[] args) => 
+        public static void Main(string[] args)
+        {
+            var config = DefaultConfig.Instance.AddDiagnoser(MemoryDiagnoser.Default);
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly)
-                .Run(args);
+                .Run(args, config);
+        }
     }
 }
