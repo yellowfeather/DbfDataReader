52b2ddf4de9a9bbe5aa23f6c19625db94ff50096
ndudas1 <47572494+ndudas1@users.noreply.github.com>
2023-05-18 12:20:40 -0600

    Update DbfValueMemo.cs to use `TryParse` rather than `Parse`.
    This handles an error case where a null character '\0' is the byte content and during parsing into the startBlock a FormatException is thrown.

--- src/DbfDataReader/DbfValueMemo.cs
+++ src/DbfDataReader/DbfValueMemo.cs
@@ -28,10 +28,13 @@ namespace DbfDataReader
                 {
                     Value = string.Empty;
                 }
+                else if (long.TryParse(value, out long startBlock))
+                {
+                    Value = _memo?.Get(startBlock);
+                }
                 else
                 {
-                    var startBlock = long.Parse(value);
-                    Value = _memo?.Get(startBlock);
+                    Value = string.Empty;
                 }
             }
         }
