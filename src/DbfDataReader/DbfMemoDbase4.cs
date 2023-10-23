368acd4407b8761c575d4f3f19482412ee919213
Joe Rybacek <joe.rybacek@outlook.com>
2023-10-23 14:11:22 -0500

    feat(memo): :sparkles: add memo support for dbase iv (#216)

--- src/DbfDataReader/DbfMemoDbase4.cs
+++ src/DbfDataReader/DbfMemoDbase4.cs
@@ -19,7 +19,17 @@ namespace DbfDataReader
 
         public override string BuildMemo(long startBlock)
         {
-            return null;
+            var offset = Offset(startBlock);
+            BinaryReader.BaseStream.Seek(offset, SeekOrigin.Begin);
+            // Read the bytes 4-7 as length of the memo field
+            BinaryReader.BaseStream.Seek(offset + 4, SeekOrigin.Begin);
+            var memoLength = BinaryReader.ReadInt32() - 8;
+            // Set at beginning of memo
+            BinaryReader.BaseStream.Seek(offset + 8, SeekOrigin.Begin);
+            var stringBuilder = new StringBuilder();
+            var block = BinaryReader.ReadString(memoLength, CurrentEncoding);
+            stringBuilder.Append(block);
+            return stringBuilder.ToString();
         }
     }
 }
