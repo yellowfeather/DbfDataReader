d26a80380844065d7d41d576bc8c72b6619da9ba
Chris Richards <chris@yellowfeather.com>
2026-02-04 23:37:17 +0000

    WIP adds dbase_02 and dbase_8c tests

---
+++ test/DbfDataReader.Tests/Dbase8CTests.cs
@@ -0,0 +1,38 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace DbfDataReader.Tests;
+
+[Collection("dbase_8C")]
+public class Dbase8CTests : DbaseTests
+{
+    private const string Dbase8CFixturePath = "../../../../fixtures/dbase_8c.dbf";
+
+    public Dbase8CTests() : base(Dbase8CFixturePath)
+    {
+    }
+
+    [Fact]
+    public void Should_report_correct_record_count()
+    {
+        DbfHeader.RecordCount.ShouldBe(8);
+    }
+
+    [Fact]
+    public void Should_report_correct_version_number()
+    {
+        DbfHeader.Version.ShouldBe(0x8B);
+    }
+
+    [Fact]
+    public void Should_have_the_correct_column_schema()
+    {
+        ValidateColumnSchema("../../../../fixtures/dbase_8c_summary.txt");
+    }
+
+    [Fact]
+    public void Should_have_correct_row_values()
+    {
+        ValidateRowValues("../../../../fixtures/dbase_8c.csv");
+    }
+}
