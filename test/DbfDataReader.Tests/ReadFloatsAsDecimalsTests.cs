b2c671ea22a1d7318c0e66c2b601f97ef047ebe8
Chris Richards <chris.richards@yellowfeather.co.uk>
2026-02-05 00:11:15 +0000

    Adds option to read floats as decimals (#251)

---
+++ test/DbfDataReader.Tests/ReadFloatsAsDecimalsTests.cs
@@ -0,0 +1,76 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace DbfDataReader.Tests;
+
+[Collection("dbase_31")]
+public class ReadFloatsAsDecimalsTests : DbaseTests
+{
+    private const string Dbase31FixturePath = "../../../../fixtures/dbase_31.dbf";
+
+    private static readonly DbfDataReaderOptions Options = new()
+    {
+        ReadFloatsAsDecimals = true
+    };
+
+    public ReadFloatsAsDecimalsTests() : base(Dbase31FixturePath, Options)
+    {
+    }
+
+    [Fact]
+    public void Should_report_correct_record_count()
+    {
+        DbfHeader.RecordCount.ShouldBe(77);
+    }
+
+    [Fact]
+    public void Should_report_correct_version_number()
+    {
+        DbfHeader.Version.ShouldBe(0x31);
+    }
+
+    [Fact]
+    public void Should_report_that_the_file_is_not_foxpro()
+    {
+        DbfHeader.IsFoxPro.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Should_have_the_correct_number_of_columns()
+    {
+        DbfTable.Columns.Count.ShouldBe(13);
+    }
+
+    [Fact]
+    public void Should_have_the_correct_column_schema()
+    {
+        ValidateColumnSchema("../../../../fixtures/dbase_31_summary.txt");
+    }
+
+    [Fact]
+    [UseCulture("en-GB")]
+    public void Should_have_correct_row_values()
+    {
+        ValidateRowValues("../../../../fixtures/dbase_31.csv");
+    }
+    
+    [Fact]
+    public void Should_read_floats_as_decimals()
+    {
+        var dbfRecord = new DbfRecord(DbfTable);
+        DbfTable.Read(dbfRecord);
+        var dbfValue = dbfRecord.Values[10];
+        dbfValue.GetValue().ShouldBeOfType<decimal>();
+    }
+    
+    [Fact]
+    public void Should_not_read_floats_as_decimals()
+    {
+        DbfTable.ReadFloatsAsDecimals = false;
+        
+        var dbfRecord = new DbfRecord(DbfTable);
+        DbfTable.Read(dbfRecord);
+        var dbfValue = dbfRecord.Values[10];
+        dbfValue.GetValue().ShouldBeOfType<float>();
+    }
+}
